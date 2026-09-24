a2a78e46193227214f69fc6c7f51227d54d1df3d
Jabro99 <jabroo271@gmail.com>
2026-09-24 14:39:58 +0100

    Fix API request

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -436,7 +436,7 @@ async function saveSpot(title, description, lat, lng) {
 
 async function logOut() {
   try {
-    const response = await fetch(`${"http://localhost:3000"}/api/logout`, {
+    const response = await fetch(`/api/logout`, {
       method: "POST",
       credentials: "include",
     });
